8a4d1182e4223b5ffe85d92cae5f91f33cbd41d8
alexandros filotheou <alexandros.filotheou@gmail.com>
2023-06-22 17:29:19 +0300

    More corrections

--- presentation_full/pdf/figures/slides/ch5/scan_to_image/centroid.m
+++
@@ -1,8 +0,0 @@
-function [xc,yc] = centroid(x,y)
-
-  area = 0.5 * sum( (shift(x,-1)+x).*(shift(y,-1)-y) );
-
-  xc = 1.0/(6*area) * sum( (shift(x,-1) + x).*(x.*shift(y,-1) - shift(x,-1).*y) );
-  yc = 1.0/(6*area) * sum( (shift(y,-1) + y).*(x.*shift(y,-1) - shift(x,-1).*y) );
-
-end
